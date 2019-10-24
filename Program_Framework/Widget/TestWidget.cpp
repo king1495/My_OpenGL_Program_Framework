ad423d3fd2630d3fafb52a1ae9e01336239299f9
JYH <hosijora@gmail.com>
2019-10-24 14:25:39 +0900

    asd

--- Program_Framework/Widget/TestWidget.cpp
+++ Program_Framework/Widget/TestWidget.cpp
@@ -70,6 +70,8 @@ void TestWidget::Update()
 		ydata2[i] = 1.5f * sin(xdata[i]);
 	}
 
+	sAxes1->xlim = { radians(t0),radians(t0 + 360.f) };
+
 	sPlot1->SetData(xdata, ydata1);
 	sPlot2->SetData(xdata, ydata2);
 }
